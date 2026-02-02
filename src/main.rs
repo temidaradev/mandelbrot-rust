429353b98db8e466a597db141b925cf6d9fc8830
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2026-02-02 23:30:28 +0300

    html and more color

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,46 @@ use macroquad::{miniquad::window, prelude::*};
 
 type Complex = num_complex::Complex<f64>;
 
-fn mandelbrot_color(c: &Complex, upper: u32) -> u32 {
+fn hsv_to_rgb(h: f64, s: f64, v: f64) -> [u8; 3] {
+    let c = v * s;
+    let x = c * (1.0 - ((h / 60.0) % 2.0 - 1.0).abs());
+    let m = v - c;
+
+    let (r, g, b) = match h {
+        h if h < 60.0 => (c, x, 0.0),
+        h if h < 120.0 => (x, c, 0.0),
+        h if h < 180.0 => (0.0, c, x),
+        h if h < 240.0 => (0.0, x, c),
+        h if h < 300.0 => (x, 0.0, c),
+        _ => (c, 0.0, x),
+    };
+
+    [
+        ((r + m) * 255.0) as u8,
+        ((g + m) * 255.0) as u8,
+        ((b + m) * 255.0) as u8,
+    ]
+}
+
+fn mandelbrot_color(c: &Complex, upper: u32) -> f64 {
     let mut z = Complex::ZERO;
 
     for i in 0..upper {
         z = z * z + c;
-        if z.norm_sqr() > 4.0 {
-            return i;
+
+        let norm_sqr = z.norm_sqr();
+        if norm_sqr > 4.0 {
+            let log_zn = norm_sqr.ln() / 2.0;
+            let nu = (log_zn / std::f64::consts::LN_2).ln() / std::f64::consts::LN_2;
+            return i as f64 + 1.0 - nu;
         }
     }
 
-    upper
+    upper as f64
 }
 
 fn mandelbrot(data: &mut [[u8; 4]], from: &Complex, to: &Complex, width: u32, height: u32) {
+    let max_iter = 255;
     for y in 0..height {
         for x in 0..width {
             let c = Complex::new(
@@ -23,10 +49,18 @@ fn mandelbrot(data: &mut [[u8; 4]], from: &Complex, to: &Complex, width: u32, he
                 from.im + (to.im - from.im) * y as f64 / height as f64,
             );
 
-            let iter = mandelbrot_color(&c, 255);
+            let mu = mandelbrot_color(&c, max_iter);
             let index = (y * width + x) as usize;
 
-            data[index] = [255 - iter as u8, 255 - iter as u8, 255 - iter as u8, 255];
+            if mu >= max_iter as f64 {
+                data[index] = [0, 0, 0, 255];
+            } else {
+                let t = (mu / max_iter as f64).powf(0.3);
+                let hue = 360.0 * t;
+
+                let rgb = hsv_to_rgb(hue, 1.0, 1.0);
+                data[index] = [rgb[0], rgb[1], rgb[2], 255];
+            }
         }
     }
 }
